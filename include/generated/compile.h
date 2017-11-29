6b6f7176417e6672cd378629d9578b0d80e8faba
riad236 <rdxriad236@gmail.com>
2017-11-29 20:13:56 +0600

    Poison-reborn

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Nov 25 21:13:56 +06 2017"
+#define LINUX_COMPILE_BY "riad"
+#define LINUX_COMPILE_HOST "Riad"
+#define LINUX_COMPILER "gcc version 4.9.4 20150720 (prerelease) (UBERTC-4.9.4) "
